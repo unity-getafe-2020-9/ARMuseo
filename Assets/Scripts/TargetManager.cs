7cced50baee8d988de76256c9aac806fab1e2a30
Fernando Paniagua <fernando.paniagua@gmail.com>
2020-11-26 14:10:34 +0100

    Initial commit

---
+++ Assets/Scripts/TargetManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PersonalApplication
+{
+    public class TargetManager : MonoBehaviour
+    {
+        bool muestraFicha = false;
+        float y;
+        private void Awake()
+        {
+            y = transform.position.y;
+        }
+        private void Update()
+        {
+            if (muestraFicha == true)
+            {
+                if (y < 1.3f)
+                {
+                    y += y + Time.deltaTime;
+                }
+                transform.position = new Vector3(transform.position.x, y, transform.position.z);
+            }
+        }
+
+        public void MuestraFicha()
+        {
+            muestraFicha = true;
+        }
+    }
+
+}
